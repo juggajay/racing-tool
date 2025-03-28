5fe48f33449eeb04a74e80e34c37fedb087ab204
juggajay <160543594+juggajay@users.noreply.github.com>
2025-03-28 12:11:06 +1100

    Add script with instructions for connecting GitHub repository to Vercel

---
+++ connect-to-vercel.js
@@ -0,0 +1,37 @@
+// Connect to Vercel Script
+// This script provides instructions on how to connect your GitHub repository to the correct Vercel project
+
+console.log('Instructions for connecting your GitHub repository to Vercel:');
+console.log('');
+console.log('1. Go to the Vercel dashboard:');
+console.log('   https://vercel.com/dashboard');
+console.log('');
+console.log('2. Navigate to your project:');
+console.log('   https://vercel.com/juggajays-projects/horses');
+console.log('');
+console.log('3. Click on the "Settings" tab');
+console.log('');
+console.log('4. Under "Git", click on "Connected Git Repository"');
+console.log('');
+console.log('5. If a repository is already connected, you may need to disconnect it first:');
+console.log('   - Click on "Disconnect"');
+console.log('   - Confirm the disconnection');
+console.log('');
+console.log('6. Click on "Connect Git Repository"');
+console.log('');
+console.log('7. Select "GitHub" as the Git provider');
+console.log('');
+console.log('8. Find and select your repository (juggajay/racing-tool)');
+console.log('');
+console.log('9. Configure the build settings:');
+console.log('   - Framework Preset: Next.js');
+console.log('   - Root Directory: ./');
+console.log('   - Build Command: next build');
+console.log('   - Output Directory: .next');
+console.log('');
+console.log('10. Click "Deploy"');
+console.log('');
+console.log('Your GitHub repository should now be connected to your Vercel project.');
+console.log('Any changes pushed to the main branch will automatically trigger a new deployment.');
+console.log('');
+console.log('For more information, visit: https://vercel.com/docs/git');
